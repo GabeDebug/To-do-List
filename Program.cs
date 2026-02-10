890508fa7825b7f9e5d8038270c9a98f240c6861
GabrielDebug <you@example.com>
2026-02-10 12:58:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace To_do_list
+{
+    class Program
+    {
+        static List<tarefa> tarefas = new List<tarefa>();
+        static int proximoId = 1;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("To-do List");
+                Console.WriteLine("1. Adicionar tarefa");
+                Console.WriteLine("2. Listar tarefas");
+                Console.WriteLine("3. tarefa concluída");
+                Console.WriteLine("4. Remover tarefa");
+                Console.WriteLine("0. Sair");
+            }
+        }
+    }
+}
---
+++ tarefa.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace To_do_list
+{
+    internal class tarefa
+    {
+        public int Id { get; set; }
+        public string descricao { get; set; }
+        public bool concluida { get; set; }
+    }
+}
