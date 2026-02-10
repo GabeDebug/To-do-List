b810c982f7f00f00eecd7cbe2114f70770e4a0ce
GabrielDebug <you@example.com>
2026-02-10 13:03:25 -0300

    create Adicionar

--- Program.cs
+++ Program.cs
@@ -16,6 +16,22 @@
                 Console.WriteLine("3. tarefa concluída");
                 Console.WriteLine("4. Remover tarefa");
                 Console.WriteLine("0. Sair");
+                Console.WriteLine("Escolha 1");
+
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1": Adicionar(); break;
+                    case "2": Listar(); break;
+                    case "3": Concluir(); break;
+                    case "4": Remover(); break;
+                    case "0": return();
+                    default:
+                        Console.WriteLine("Opção inválida. Pressione Enter para continuar.");
+                        Console.ReadLine();
+                        break;
+                }
             }
         }
     }
